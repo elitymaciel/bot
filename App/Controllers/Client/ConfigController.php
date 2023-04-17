e7515d8acf03e7bf9b268a529617285da0940ac5
elitymaciel <elitymaciel@hotmail.com>
2023-04-17 13:30:05 -0300

    configuraçoes de pasta

--- App/Controllers/Client/ConfigController.php
+++ App/Controllers/Client/ConfigController.php
@@ -33,20 +33,20 @@ class ConfigController extends ClientController
         $status = new \App\Helpers\Api();
         $usuario = User::where('email', $_SESSION['email'])->first();
         $api = Api::where('user_id', $usuario->id)->first();
-        $session = Session::query('session', $api->session)->first();
+        $session = Session::query('id_session', $api->id)->first();
         if($session){
-            Session::query('session', $api->session)->update([ 
+            Session::query('id_session', $api->id)->update([ 
                 'type' => 'loding',
                 'urlcode' => 's'
             ]);
-            echo  $status->start(['token' => $usuario->token, 'session' => $api->session]);
+            echo  $status->start(['token' => $api->token, 'session' => $api->session]);
         }else {
-            Session::query('session', $api->session)->create([ 
-                'id_session' => $api->session,
+            Session::query('id_session', $api->session)->create([ 
+                'id_session' => $api->id,
                 'type' => 'loding',
                 'urlcode' => 's'
             ]);
-            echo  $status->start(['token' => $usuario->token, 'session' => $api->session]);
+            echo  $status->start(['token' => $api->token, 'session' => $api->session]);
         }
     }
     
@@ -54,14 +54,13 @@ class ConfigController extends ClientController
     {
         $usuario = User::where('email', $_SESSION['email'])->first();
         $api = Api::where('user_id', $usuario->id)->first();
-        $qrcode = Session::where('id_session', $api->session)->first();
+        $qrcode = Session::where('id_session', $api->id)->first();
         if($qrcode){
             if($qrcode->urlcode > 0){
                 echo  json_encode(['status' => $qrcode->type, 'qrcode' => $qrcode->urlcode]);
             }else{
                 echo  json_encode(['status' => $qrcode->type, 'qrcode' => 'sem qrcode']);
-            }
-            
+            } 
         }else{
             echo  json_encode(['status' => 'errorSession', 'menssage' => 'Nao possui session iniciada']);
         }
