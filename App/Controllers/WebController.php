1ed15812ec48b099c46706efc5697ea8ec3e063d
elitymaciel <elitymaciel@hotmail.com>
2023-04-19 19:59:32 -0300

    correcao de testes

--- App/Controllers/WebController.php
+++ App/Controllers/WebController.php
@@ -9,14 +9,6 @@ class WebController extends Controller
 {
     public function index()
     {
-        $menu = Mensagem::distinct()->pluck('categoria');
-            $options = [];
-            foreach ($menu as $key => $categoria) {
-                $options[] = $key+1 .'- ' .ucfirst($categoria);
-            } 
-        echo "<pre>";
-        print_r($options);
-        die;
         echo $this->view()->render("home");
     }
 }
